39701a3f43f405d215b1f04f68fb6ad2beeb7370
Ploy <117616170+yaminP@users.noreply.github.com>
2022-12-01 12:29:26 +0700

    ass

--- pages/api/about/index.js
+++ pages/api/about/index.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
     switch (method) {
         case 'GET':
             try {
-                const data = await prisma.about.findFirst();
+                const data = await prisma.about.findMany();
                 res.status(200).json(data)
             } catch (error) {
                 res.status(400).json({ success: false })
@@ -16,15 +16,11 @@ export default async function handler(req, res) {
         case 'POST':
             try {
                 await prisma.about.update({
-                    where: {
-                        id: req.query.id
-                    },
                     data: {
-                        title: req.query.title,
-                        subtitle: req.query.subtitle,
-                        detail: req.query.detail,
-                        image: req.query.image,
-
+                        title: req.body.title,
+                        subtitle: req.body.subtitle,
+                        detail: req.body.detail,
+                        image: req.body.image,
                     }
                 })
                 prisma.$disconnect();
