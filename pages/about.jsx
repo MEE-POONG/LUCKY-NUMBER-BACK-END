e4c97d2c4289a4e3c8501ea68e4aa5410c388abd
Ploy <117616170+yaminP@users.noreply.github.com>
2022-12-01 11:38:31 +0700

    abbp

--- pages/about.jsx
+++ pages/about.jsx
@@ -7,8 +7,7 @@ import Editor from '@/components/Ckeditor/Editor';
 import useAxios from 'axios-hooks';
 
 export default function AboutPage() {
-
-    const [{data: aboutData}] = useAxios({url: '/api/about'})
+    const [{data: aboutData, loading, error}, getAbout] = useAxios({url: '/api/about'})
     const [{ data: aboutById , loading: aboutByIdLoading , error: aboutByIdError},getAboutById] = useAxios({},{ manual: true } )
     const [{ loading: updateAboutLoading, error: updateAboutError }, executeAboutPut] = useAxios({},{manual: true})
 
@@ -51,41 +50,42 @@ export default function AboutPage() {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <Container fluid className=" pt-4 px-4">
+            {aboutById?.map((about, index) =>(
+            <Container fluid className=" pt-4 px-4" key={index}>
                 <Row className=" g-4">
-
                     <div className="bg-secondary rounded p-4">
 
                         <h6 className="mb-4"> ข้อมูลร้าน </h6>
-                        <form>
-                            <div className="mb-3">
-                                <label for="Inputname" className="form-label">รูปภาพของร้าน</label><br />
-                                {imageURL.map(imageSrcProduct => <Image className="mb-2" style={{ height: 200 }} src={imageSrcProduct} alt="product_img" fluid rounded />)}
-                                <input type="file" accept="image/*" onChange={onImageProductChange} className="form-control" id="#" placeholder='' />
-                                <br />
-                                    <div className="mb-3">
-                                        <label for="Inputphone" className="form-label">หัวข้อหลัก</label>
-                                        <input type="tel" className="form-control" style={{ width: "500px" }} id="# " placeholder='Lucky Number' />
-                                    </div>
-
-                                    <div className="mb-3">
-                                        <label for="Inputphone" className="form-label">หัวข้อหลัก</label>
-                                        <input type="tel" className="form-control" style={{ width: "500px" }} id="# " placeholder='Lucky Number' />
-                                    </div>
-
-                                <div className="mb-3">
-                                    <label for="Inputphone" className="form-label">รายละเอียดร้าน</label>
-                                    <Editor/>
-                                </div>
-                            </div>
-
-                            <button type="submit" className="btn btn-success"  >ยืนยัน</button>
-                        </form>
+                        
+                             <form >
+                             <div className="mb-3">
+                                 <label for="Inputname" className="form-label">รูปภาพของร้าน</label><br />
+                                 <Image className="logo" style={{ borderRadius: "10px" }} src={about.Image} />
+                                 <br />
+                                     <div className="mb-3">
+                                         <label for="Inputphone" className="form-label">หัวข้อหลัก</label>
+                                         <h3>{about.title}</h3>
+                                     </div>
+ 
+                                     <div className="mb-3">
+                                         <label for="Inputphone" className="form-label">หัวข้อหลัก</label>
+                                         <h5>{about.subtitle}</h5>
+                                     </div>
+ 
+                                 <div className="mb-3">
+                                     <label for="Inputphone" className="form-label">รายละเอียดร้าน</label>
+                                     <p>{about.detail}</p>
+                                     <Editor/>
+                                 </div>
+                             </div>
+ 
+                             <button type="submit" className="btn btn-success"  >ยืนยัน</button>
+                         </form>
+                    
                     </div>
                 </Row>
-
-
             </Container>
+            ))}
         </ >
     );
 }
