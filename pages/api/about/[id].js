441562d0abcbe8fb792228b5b237701d82def89a
Ploy <117616170+yaminP@users.noreply.github.com>
2022-12-01 09:20:22 +0700

    opp

--- pages/api/about/[id].js
+++ pages/api/about/[id].js
@@ -2,18 +2,22 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 export default async function handler(req, res) {
-    // console.log("",req.body);
     const { method } = req
     switch (method) {
         case 'GET':
             try {
-                const data = await prisma.about.findFirst();
+                const data = await prisma.about.findFirst({
+                    where: {
+                        id: req.query.id
+                    }
+                });
+                prisma.$disconnect();
                 res.status(200).json(data)
             } catch (error) {
                 res.status(400).json({ success: false })
             }
             break
-        case 'POST':
+        case 'PUT':
             try {
                 await prisma.about.update({
                     where: {
